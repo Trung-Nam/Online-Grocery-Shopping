e220791d2ed5d1db6f50e7be280a49fc0e8ad96b
Trung Nam <trungnam.work@gmail.com>
2024-10-02 19:10:20 +0700

    feat(ui, root css): defind root css and add dropdown all categories

--- src/Components/Header/Navigation.jsx
+++ src/Components/Header/Navigation.jsx
@@ -1,27 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosMenu } from 'react-icons/io';
-import { FaAngleDown } from 'react-icons/fa6';
+import { FaAngleDown, FaAngleRight } from 'react-icons/fa6';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 const Navigation = () => {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
     return (
-        <div className="header-nav d-flex align-items-center">
+        <div className="header-nav d-flex align-items-center border-bottom">
             <div className="container">
                 <div className="row">
                     <div className="col-sm-3 all-categories">
-                        <Button className='all-categories-btn align-items-center'>
-                            <span className='menu-icon me-2'>
-                                <IoIosMenu />
-                            </span>
-                            <span>All Categories</span>
-                            <span className='angle-down-icon ms-2'>
-                                <FaAngleDown />
-                            </span>
-                        </Button>
-                        <div className='all-categories-description'>
-                            TOTAL 63 PRODUCTS
+                        <div className="dropdown">
+                            <Button
+                                className="all-categories-btn align-items-center"
+                                type="button"
+                                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                                aria-expanded="false"
+                            >
+                                <span className="menu-icon me-2">
+                                    <IoIosMenu />
+                                </span>
+                                <span>All Categories</span>
+                                <span className="angle-down-icon ms-2">
+                                    <FaAngleDown />
+                                </span>
+                            </Button>
+                            {isDropdownOpen && (
+                                <ul className="dropdown-menu custom-dropdown-menu">
+                                    <li>
+                                        <a className="dropdown-item d-flex align-items-center" href="/">
+                                            <img src={require('../../assets/images/vegetable.png')} alt="vegetable-icon" />
+                                            Fruits & Vegetables
+                                            <FaAngleRight />
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item d-flex align-items-center" href="/">
+                                            <img src={require('../../assets/images/barbecue.png')} alt="meat-icon" />
+                                            Meats & Seafood
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item d-flex align-items-center" href="/">
+                                            <img src={require('../../assets/images/english-breakfast.png')} alt="breakfast-icon" />
+                                            Breakfast & Dairy
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item d-flex align-items-center" href="/">
+                                            <img src={require('../../assets/images/healthy-drink.png')} alt="healthy-drink-icon" />
+                                            Beverages
+                                            <FaAngleRight />
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item d-flex align-items-center" href="/">
+                                            <img src={require('../../assets/images/bread.png')} alt="bread-icon" />
+                                            Breads & Bakery
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item d-flex align-items-center" href="/">
+                                            <img src={require('../../assets/images/frozen-food.png')} alt="frozen-food-icon" />
+                                            Frozen Foods
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item" href="/">
+                                            <img src={require('../../assets/images/bar.png')} alt="bar-icon" />
+                                            Biscuits & Snacks
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item" href="/">
+                                            <img src={require('../../assets/images/basket.png')} alt="basket-icon" />
+                                            Grocery & Staples
+                                        </a>
+                                    </li>
+                                </ul>
+                            )}
                         </div>
+                        <div className="all-categories-description">TOTAL 63 PRODUCTS</div>
                     </div>
+
 
                     <div className="col-sm-9 primary-menu d-flex align-items-center">
                         <ul className='list-group list-group-horizontal ms-auto'>

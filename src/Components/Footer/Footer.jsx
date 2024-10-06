d8d95c4b54b0cfd8ca78726196ee417666e6e229
Trung Nam <trungnam.work@gmail.com>
2024-10-06 16:18:55 +0700

    feat(ui): footer-widgets

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -107,6 +107,187 @@ const Footer = () => {
                 </div>
             </div>
 
+            <div className="footer-widgets">
+                <div className="container">
+                    <div className="row d-flex justify-content-between">
+                        <div className="col col-12 col-lg-2">
+                            <div className="widget_nav_menu">
+                                <h4 className="widget-title">FRUIT &amp; VEGETABLES</h4>
+                                <div className="menu-fruits-and-vegetables-container">
+                                    <ul id="menu-fruits-and-vegetables" className="menu">
+                                        <li className="menu-item">
+                                            <a href='/'>Fresh Vegetables</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href='/'>Herbs &amp; Seasonings</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href='/'>Fresh Fruits</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href='/'>Cuts &amp; Sprouts</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href='/'>Exotic Fruits &amp; Veggies</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href='/'>Packaged Produce</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href='/'>Party Trays</a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div className="col col-12 col-lg-2">
+                            <div className="widget widget_nav_menu">
+                                <h4 className="widget-title">Breakfast &amp; Dairy</h4>
+                                <div className="menu-breakfast-dairy-container">
+                                    <ul id="menu-breakfast-dairy" className="menu">
+                                        <li id="menu-item-1807" className="menu-item">
+                                            <a href="/">Milk &amp; Flavoured Milk</a>
+                                        </li>
+                                        <li id="menu-item-1802" className="menu-item">
+                                            <a href="/">Butter and Margarine</a>
+                                        </li>
+                                        <li id="menu-item-1803" className="menu-item">
+                                            <a href="/">Cheese</a>
+                                        </li>
+                                        <li id="menu-item-1804" className="menu-item">
+                                            <a href="/">Eggs Substitutes</a>
+                                        </li>
+                                        <li id="menu-item-1805" className="menu-item">
+                                            <a href="/">Honey</a>
+                                        </li>
+                                        <li id="menu-item-1806" className="menu-item">
+                                            <a href="/">Marmalades</a>
+                                        </li>
+                                        <li id="menu-item-1808" className="menu-item">
+                                            <a href="/">Sour Cream and Dips</a>
+                                        </li>
+                                        <li id="menu-item-1809" className="menu-item">
+                                            <a href="/">Yogurt</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col col-12 col-lg-2">
+                            <div className="widget widget_nav_menu">
+                                <h4 className="widget-title">Meat &amp; Seafood</h4>
+                                <div className="menu-meat-seafood-container">
+                                    <ul id="menu-meat-seafood" className="menu">
+                                        <li className="menu-item">
+                                            <a href="/">Breakfast Sausage</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Dinner Sausage</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Beef</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Chicken</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Sliced Deli Meat</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Shrimp</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Wild Caught Fillets</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Crab and Shellfish</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Farm Raised Fillets</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col col-12 col-lg-2">
+                            <div className="widget widget_nav_menu">
+                                <h4 className="widget-title">Beverages</h4>
+                                <div className="menu-beverages-container">
+                                    <ul id="menu-beverages" className="menu">
+                                        <li className="menu-item">
+                                            <a href="/">Water</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Sparkling Water</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Soda &amp; Pop</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Coffee</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Milk &amp; Plant-Based Milk</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Tea &amp; Kombucha</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Drink Boxes &amp; Pouches</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Craft Beer</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Wine</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col col-12 col-lg-2">
+                            <div className="widget widget_nav_menu">
+                                <h4 className="widget-title">Breads &amp; Bakery</h4>
+                                <div className="menu-breakfast-dairy-container">
+                                    <ul id="menu-breakfast-dairy-1" className="menu">
+                                        <li className="menu-item">
+                                            <a href="/">Milk &amp; Flavoured Milk</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Butter and Margarine</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Cheese</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Eggs Substitutes</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Honey</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Marmalades</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Sour Cream and Dips</a>
+                                        </li>
+                                        <li className="menu-item">
+                                            <a href="/">Yogurt</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
         </footer>
     )
 }
